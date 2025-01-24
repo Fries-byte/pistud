70977443f4aa0578881296a959bae534fe2a046e
Fries-Byte <69freshfrenchfries@gmail.com>
2025-01-24 13:42:42 +0100

    Create Input_ver2.py

---
+++ tests/Input_ver2.py
@@ -0,0 +1,16 @@
+import sys
+import os
+src_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../src"))
+sys.path.append(src_path)
+import p
+
+
+# Testing the new input system
+p.cv("variable", p.iln("Hello"))
+p.pln("variable")
+
+# Expected output:
+# (user input)
+#
+
+# Result: No Errors
