7ae9f40c19edb25ac53373af3400d3ce00984071
Fries-Byte <69freshfrenchfries@gmail.com>
2025-02-01 12:18:14 +0100

    Update packages.py

--- packages.py
+++ packages.py
@@ -1,20 +1,20 @@
 import urllib.request
 if name == "versions":
   if version == 6.5:
-    exec(urllib.request.urlopen("https://raw.githubusercontent.com/year-Lang/pust/refs/heads/main/src/yr.py").read().decode())
+    exec(urllib.request.urlopen("https://raw.githubusercontent.com/PiStud-Lang/PiStud/refs/heads/main/src/yr.py").read().decode())
   elif version == 6:
-    exec(urllib.request.urlopen("https://raw.githubusercontent.com/year-Lang/pust/refs/heads/main/src/ps_ver6.py").read().decode())
+    exec(urllib.request.urlopen("https://raw.githubusercontent.com/PiStud-Lang/PiStud/refs/heads/main/src/ps_ver6.py").read().decode())
   elif version == 5:
-    exec(urllib.request.urlopen("https://raw.githubusercontent.com/year-Lang/pust/refs/heads/main/src/ps_ver5.py").read().decode())
+    exec(urllib.request.urlopen("https://raw.githubusercontent.com/PiStud-Lang/PiStud/refs/heads/main/src/ps_ver5.py").read().decode())
   elif version == 4:
-    exec(urllib.request.urlopen("https://github.com/year-Lang/pust/raw/refs/heads/main/src/ps_ver4.py").read().decode())
+    exec(urllib.request.urlopen("https://github.com/PiStud-Lang/PiStud/raw/refs/heads/main/src/ps_ver4.py").read().decode())
   elif version == 3:
-    exec(urllib.request.urlopen("https://github.com/year-Lang/pust/raw/refs/heads/main/src/ps_ver3.py").read().decode())
+    exec(urllib.request.urlopen("https://github.com/PiStud-Lang/PiStud/raw/refs/heads/main/src/ps_ver3.py").read().decode())
   elif version == 2:
-    exec(urllib.request.urlopen("https://github.com/year-Lang/pust/raw/refs/heads/main/src/ps_ver2.py").read().decode())
+    exec(urllib.request.urlopen("https://github.com/PiStud-Lang/PiStud/raw/refs/heads/main/src/ps_ver2.py").read().decode())
   elif version == 1:
-    exec(urllib.request.urlopen("https://raw.githubusercontent.com/year-Lang/pust/refs/heads/main/src/ps_ver1.py").read().decode())
+    exec(urllib.request.urlopen("https://raw.githubusercontent.com/PiStud-Lang/PiStud/refs/heads/main/src/ps_ver1.py").read().decode())
     
 if name == "opack":
     if version == 1:
-      exec(urllib.request.urlopen("https://raw.githubusercontent.com/year-Lang/pust/refs/heads/main/src/ps_ver1.py").read().decode())
+      exec(urllib.request.urlopen("https://raw.githubusercontent.com/PiStud-Lang/PiStud/refs/heads/main/src/ps_ver1.py").read().decode())
