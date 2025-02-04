7d720b91ceadf05ba05406f181ea82224eca7b0f
Fries-Byte <69freshfrenchfries@gmail.com>
2025-02-04 18:57:07 +0100

    Update packages.py

--- packages.py
+++ packages.py
@@ -11,11 +11,11 @@ if name == "versions":
   elif version == 5:
     exec(urllib.request.urlopen("https://raw.githubusercontent.com/PiStud-Lang/PiStud/refs/heads/main/src/ps_ver5.py").read().decode())
   elif version == 4:
-    exec(urllib.request.urlopen("https://github.com/PiStud-Lang/PiStud/raw/refs/heads/main/src/ps_ver4.py").read().decode())
+    exec(urllib.request.urlopen("https://raw.githubusercontent.com/PiStud-Lang/PiStud/refs/heads/main/src/ps_ver4.py").read().decode())
   elif version == 3:
-    exec(urllib.request.urlopen("https://github.com/PiStud-Lang/PiStud/raw/refs/heads/main/src/ps_ver3.py").read().decode())
+    exec(urllib.request.urlopen("https://raw.githubusercontent.com/PiStud-Lang/PiStud/refs/heads/main/src/ps_ver3.py").read().decode())
   elif version == 2:
-    exec(urllib.request.urlopen("https://github.com/PiStud-Lang/PiStud/raw/refs/heads/main/src/ps_ver2.py").read().decode())
+    exec(urllib.request.urlopen("https://raw.githubusercontent.com/PiStud-Lang/PiStud/refs/heads/main/src/ps_ver2.py").read().decode())
   elif version == 1:
     exec(urllib.request.urlopen("https://raw.githubusercontent.com/PiStud-Lang/PiStud/refs/heads/main/src/ps_ver1.py").read().decode())
     
